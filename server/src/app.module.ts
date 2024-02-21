476b56b64f609340558f92a465038aa82e78d0f5
Andrey <raigon28@gmail.com>
2024-02-21 16:36:26 +0300

    feat: add config and connect db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,31 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
+  imports: [
+    UserModule,
+    CategoryModule,
+    AuthModule,
+    TransactionModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        database: configService.get('DATABASE_NAME'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity{.js, .ts}'],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
